93a2d61c296c7ab38e9c3523729392c1ac5f4fb0
Nikhil Pannala <nikhilreddypannala.98@gmail.com>
2021-08-19 14:56:04 -0500

    added demo1

---
+++ Demo1.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+var i = 9
+var j = 10
+var sum = i + j
+print(sum)
+
+var  k = 5.2
+var v = 3.5
+var product = k*v
+print(product)
+
+var p = 7
+var n = 3
+var sub = p-n
+print(sub)
